8d988d529096ed981fea68541cfd3c3110aa8ffd
Victoria Hale <vic.hale74@gmail.com>
2019-09-10 02:45:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,39 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_array = ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+    array << element
+p array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   array = ["I", "am", "really", "learning"]
+    element = "wow"
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop["arrays!"]
+
 end
 
 def remove_element_from_start_of_array(array)
+   array = ["wow", "I", "am", "really", "learning", "arrays!"]
+   array.shift["wow"]
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
